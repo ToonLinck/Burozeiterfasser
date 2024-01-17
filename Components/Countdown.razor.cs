11f2d70e760d9ef4e6ed2b31589f2f49adf97ded
JLinck <johanlinck96@gmail.com>
2024-01-17 17:16:52 +0100

    init

---
+++ Components/Countdown.razor.cs
@@ -0,0 +1,66 @@
+﻿using System.Timers;
+using Microsoft.AspNetCore.Components;
+
+namespace Bürozeiterfasser.Components
+{
+    public partial class Countdown
+    {
+        public TimeSpan CountdownTime { get; set; }
+
+        private System.Timers.Timer timer;
+        private bool timeOver = false;
+
+        [Parameter]
+        public DateTime WorkStart { get; set; }
+        [Parameter]
+        public DateTime WorkEnd { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+
+            base.OnInitialized();
+        }
+
+        private void SetTimer()
+        {
+            timer = new System.Timers.Timer(100);
+            timer.Elapsed += async (sender, e) => await CountdownTick();
+            timer.Enabled = true;
+        }
+
+        private void StopTimer()
+        {
+            timer.Enabled = false;
+        }
+
+
+        public void StartCountdown()
+        {
+            timeOver = false;
+
+            CountdownTime = WorkEnd - WorkStart;
+
+            SetTimer();
+
+            StateHasChanged();
+        }
+
+
+        private async Task CountdownTick()
+        {
+            if (CountdownTime.Equals(TimeSpan.Zero))
+            {
+                timeOver = true;
+                StopTimer();
+
+            }
+            else
+            {
+                CountdownTime = CountdownTime.Add(-TimeSpan.FromSeconds(1));
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using Bürozeiterfasser.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Bürozeiterfasser.Pages
+{
+    public partial class Index
+    {
+        private TimeSpan workTime;
+        private DateTime workStart, workEnd;
+        private int workTimeHour, workTimeMinute;
+
+        private Countdown coundownRef { get; set; }
+
+        private string timeZoneSelected { get; set; }
+
+        private async Task UpdateWorktime()
+        {
+            StateHasChanged();
+
+            workTime = new TimeSpan(workTimeHour,workTimeMinute,0);
+
+            workEnd = workStart.Add(workTime);
+
+            coundownRef.StartCountdown();
+
+            StateHasChanged();
+        }
+
+        private async Task TimezoneChanged()
+        {
+            workStart = TimeZoneInfo.ConvertTime(workStart, getSelectedTimeZone());
+        }
+
+
+        private TimeZoneInfo getSelectedTimeZone()
+        {
+            switch(timeZoneSelected)
+            {
+                case "cet":
+                    return TimeZoneInfo.CreateCustomTimeZone("cet", new TimeSpan(1,0,0),"CET","Berlin");
+
+                case "est":
+                    return TimeZoneInfo.CreateCustomTimeZone("est", new TimeSpan(-5, 0, 0), "EST", "NewYork");
+
+                case "jst":
+                    return TimeZoneInfo.CreateCustomTimeZone("jst", new TimeSpan(9, 0, 0), "JST", "Tokyo");
+
+                default: return TimeZoneInfo.CreateCustomTimeZone("cet", new TimeSpan(1, 0, 0), "CET", "Berlin"); ;
+                    break;
+                    
+            }
+        }
+    }
+}
